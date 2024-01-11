c22e51a60f20700ccd37077c84e7cf22aa578b4e
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2024-01-11 18:10:21 +0000

    Feature: Add new project for ReactApp.

--- ReactApp/reactapp.client/vite.config.js
+++ ReactApp/reactapp.client/vite.config.js
@@ -47,7 +47,7 @@ export default defineConfig({
     server: {
         proxy: {
             '^/weatherforecast': {
-                target: 'https://localhost:7056/',
+                target: 'https://localhost:7204/',
                 secure: false
             }
         },
