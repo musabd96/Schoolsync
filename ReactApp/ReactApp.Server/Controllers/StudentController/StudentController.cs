b3ffd17c44a5419bcdb6c19125bd00bf11513408
Robert Svart <robert.svart@student.nbi-handelsakademin.se>
2024-01-16 18:51:37 +0100

    Controller Get all students done and installed prettier to format our code

--- ReactApp/ReactApp.Server/Controllers/StudentController/StudentController.cs
+++ ReactApp/ReactApp.Server/Controllers/StudentController/StudentController.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Queries.Students.GetAllStudents;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
 
 namespace ReactApp.Server.Controllers.StudentController
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class StudentController : Controller
     {
-
+        private readonly IMediator _mediator;
+        public StudentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        //Get all Students
+        [HttpGet]
+        [Route("getAllStudents")]
+        public async Task<IActionResult> GetAllStudents()
+        {
+            try
+            {
+                var query = new GetAllStudentsQuery();
+                var result = await _mediator.Send(query);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
     }
 }
