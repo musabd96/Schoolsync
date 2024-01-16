3b6510c6c089126f4e658101ffa7aa8cfbdb8aac
musabd96 <nageyejr96@gmail.com>
2024-01-16 18:02:59 +0100

    Bug: Delete all controllers methods.

--- ReactApp/ReactApp.Server/Controllers/StudentController/StudentController.cs
+++ ReactApp/ReactApp.Server/Controllers/StudentController/StudentController.cs
@@ -1,87 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ReactApp.Server.Controllers.StudentController
 {
     public class StudentController : Controller
     {
 
-        //GetAllStudents
-        [HttpGet]
-        [Route("GetAllStudents")]
-        public ActionResult Getall(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-
-        [HttpGet]
-        [Route("GetById")]
-        public ActionResult GetById(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // AddStudents
-        [HttpPost]
-        [Route("addNewStudent")]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-
-        //UpdateStudent
-        [HttpPost]
-        [Route("updateStudent")]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //Delete Student
-        [HttpPost("{id}")]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
