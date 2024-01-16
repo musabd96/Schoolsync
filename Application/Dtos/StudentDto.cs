4a3049dc1107dcd56e9aaca8e7b57da48022f169
Nour Solaiman <solaiman.nour@gmail.com>
2024-01-16 13:36:49 +0100

    Added StudentId and StudentName in StudentModel and added StudentName in StudentDto

--- Application/Dtos/StudentDto.cs
+++ Application/Dtos/StudentDto.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Dtos
+﻿namespace Application.Dtos
 {
-    internal class StudentDto
+	public class StudentDto
     {
+        public string StudentName { get; set; } = string.Empty;
     }
 }
--- Domain/Models/Student/Student.cs
+++ Domain/Models/Student/Student.cs
@@ -2,5 +2,8 @@
 {
     public class Student
     {
+        public Guid StudentId { get; set; }
+        public string StudentName { get; set;} = string.Empty;
+
     }
 }
