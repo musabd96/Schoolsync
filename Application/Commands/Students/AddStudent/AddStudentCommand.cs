231a0c5de4a76347b37196bc6cd2acb837dc3e91
Hüseyin Sürer <huseyin.surer@student.nbi-handelsakademin.se>
2024-01-19 14:10:39 +0100

    skapat query handler för add student och lagy till add student inuti controllers

--- Application/Commands/Students/AddStudent/AddStudentCommand.cs
+++ Application/Commands/Students/AddStudent/AddStudentCommand.cs
@@ -1,6 +1,16 @@
-﻿namespace Application.Commands.Students.AddStudent
+﻿
+using MediatR;
+using System;
+
+namespace Application.Commands.Students.AddStudent
 {
-    public class AddStudentCommand
+    public class AddStudentCommand : IRequest<Guid>
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
--- Application/Commands/Students/AddStudent/AddStudentCommandHandler.cs
+++ Application/Commands/Students/AddStudent/AddStudentCommandHandler.cs
@@ -1,6 +1,40 @@
-﻿namespace Application.Commands.Students.AddStudent
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain.Models.Student;
+using MediatR;
+
+namespace Application.Commands.Students.AddStudent
 {
-    public class AddStudentCommandHandler
+    public class AddStudentCommandHandler : IRequestHandler<AddStudentCommand, Guid>
     {
+        private readonly AppDbContext _context;
+
+        public AddStudentCommandHandler(AppDbContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<Guid> Handle(AddStudentCommand request, CancellationToken cancellationToken)
+        {
+            // Skapa en ny student
+            var newStudent = new Student
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                DateOfBirth = request.DateOfBirth,
+                Address = request.Address,
+                PhoneNumber = request.PhoneNumber,
+                Email = request.Email
+            };
+
+            // Lägg till studenten i kontexten och spara ändringarna i databasen
+            _context.Students.Add(newStudent);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            // Returnera studentens ID efter att den har lagts till
+            return newStudent.Id;
+        }
     }
 }
