96d5dc25da7362448e86bcc639b198184829fdc4
musabd96 <mustafa.abdulle@student.nbi-handelsakademin.se>
2024-01-20 15:38:40 +0000

    Its done husky

--- ReactApp/ReactApp.Server/Program.cs
+++ ReactApp/ReactApp.Server/Program.cs
@@ -12,7 +12,6 @@ namespace ReactApp.Server
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
