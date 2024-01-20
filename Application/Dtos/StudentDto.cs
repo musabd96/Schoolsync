86f421b86245f2851a16dd7f326f9fa1d7671a66
musabd96 <nageyejr96@gmail.com>
2024-01-20 13:40:36 +0100

    Feature: appsetting.json connectionstring for database. files in gitignore add migration folder and appsetting.json file.  address property in person model correct spelling

--- Application/Dtos/StudentDto.cs
+++ Application/Dtos/StudentDto.cs
@@ -4,8 +4,8 @@
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public DateTime DateOfBirth { get; set; }
-        public string Adress { get; set; } = string.Empty;
+        public DateOnly DateOfBirth { get; set; }
+        public string Address { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
     }
