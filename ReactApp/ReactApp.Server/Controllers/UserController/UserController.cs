ebf76dc2834d859b12a67c43018baa6b937c5ee3
Peppe <patrik.hallqvist@student.nbi-handelsakademin.se>
2024-01-23 20:12:14 +0100

    apictonriollerertertert (felstavat)

--- ReactApp/ReactApp.Server/Controllers/UserController/UserController.cs
+++ ReactApp/ReactApp.Server/Controllers/UserController/UserController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ReactApp.Server.Controllers.UserController
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : Controller
     {
         internal readonly IMediator _mediator;
