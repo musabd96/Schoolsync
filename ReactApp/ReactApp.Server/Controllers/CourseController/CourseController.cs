1528de11075c8344cbe49215d1b5d3813f21fad3
Susu S <ssumayasofaa@gmail.com>
2024-01-29 19:46:17 +0100

    Jag har lagt till alla filer som är kvar.

---
+++ ReactApp/ReactApp.Server/Controllers/CourseController/CourseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReactApp.Server.Controllers.CourseController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : Controller
+    {
+        internal readonly IMediator _mediator;
+
+        public CourseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+    }
+}
