1a2224be121d08970089cb90f6a9803b9a0543e5
Hüseyin Sürer <huseyin.surer@student.nbi-handelsakademin.se>
2024-01-30 21:07:21 +0100

    created addclassroom feature

--- Application/Commands/Classrooms/AddClassroom/AddClassroomCommand.cs
+++ Application/Commands/Classrooms/AddClassroom/AddClassroomCommand.cs
@@ -1,6 +1,17 @@
-﻿namespace Application.Commands.Classrooms.AddClassroom
+﻿using Application.Dtos;
+using Domain.Models.Classrooms;
+using MediatR;
+using System;
+
+namespace Application.Commands.Classrooms.AddClassroom
 {
-    public class AddClassroomCommand
+    public class AddClassroomCommand : IRequest<Classroom>
     {
+        public AddClassroomCommand(ClassroomDto classroom)
+        {
+            Classroom = classroom;
+        }
+
+        public ClassroomDto Classroom { get; set; }
     }
 }
--- Infrastructure/Repositories/Classrooms/IClassroomRepository.cs
+++ Infrastructure/Repositories/Classrooms/IClassroomRepository.cs
@@ -1,10 +1,13 @@
 ﻿using Domain.Models.Classrooms;
+using Domain.Models.Student;
 
 namespace Infrastructure.Repositories.Classrooms
 {
     public interface IClassroomRepository
     {
         Task<List<Classroom>> GetAllClassrooms(CancellationToken cancellationToken);
+
+        Task<Classroom> AddClassroom(Classroom newClassroom, CancellationToken cancellationToken);
         Task<Classroom> UpdateClassroom(Guid id, string ClassroomName, CancellationToken cancellationToken);
     }
 }
